175ab4c207a903fd3427e5676765e2ecc44675b2
Adedayo UWensuyi <onehpee@yahoo.com>
2025-03-22 23:45:25 -0500

    creating number diffuculty

---
+++ main.py
@@ -0,0 +1,31 @@
+def play_round():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    print("You have 5 chances to guess the correct number.")
+
+    response = input("Please select the difficulty level:")
+    game_difficulty = response == ""
+    print("1. Easy (10 chances)")
+    print("2. Medium (5 chances)")
+    print("3. Hard (3 chances)")
+    
+    print("Round ended.")
+    return True  # Or return False to stop the game
+
+def main():
+    play_again = True
+    while play_again:
+        if not play_round():
+            break # Exit loop if play round returns False
+        
+        response = input("Play another round? (yes/no): ").lower()
+        play_again = response == "yes"
+
+    print("Game over.")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
